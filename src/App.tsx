8cfb9fbdc57e7e81a500fc40c571ed209d6a1748
Eric Chan <khii_ec@hotmail.com>
2021-08-13 10:16:10 -0700

    Init boilerplate code

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Typography } from '@material-ui/core';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Typography variant='h4'>Hello World!</Typography>
+    </div>
+  );
+}
+
+export default App;
